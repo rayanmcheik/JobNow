a7721b3ada8ea39fbcebeb1336ec6baaa7de87d5
rayanmcheik <rayan.m.mcheik@gmail.com>
2025-11-05 10:43:34 +0200

    done

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { FaChartBar } from 'react-icons/fa';
+
 
 export default function Header() {
   return (
@@ -17,14 +17,14 @@ export default function Header() {
       <div className="relative flex flex-row w-full h-auto mx-auto max-w-[1200px]">
 
         <div className="flex flex-col w-1/2">
-          <div className="flex justify-between w-full h-auto px-6 pt-10 ">
+          <div className="flex justify-between w-full h-auto px-6 pt-16 ">
             <div className="flex items-center">
               <Link href="/">
-                <img src="logo.webp" alt="Logo" className="w-auto h-7" />
+                <img src="logo.svg" alt="Logo" className="w-auto h-7" />
               </Link>
             </div>
 
-            <nav className="items-center hidden space-x-10 text-sm font-bold text-black md:flex">
+            <nav className="items-center hidden space-x-10 text-[16px]  text-black md:flex">
               <a href="#" className="hover:text-black">Home</a>
               <a href="#" className="hover:text-black">Job</a>
               <a href="#" className="hover:text-black">About Us</a>
@@ -32,15 +32,15 @@ export default function Header() {
             </nav>
           </div>
 
-          <div className="flex flex-col items-start justify-center w-full h-full px-6 pt-20 ">
-            <h1 className="text-4xl font-bold pb-7 font-momo">Search, Find, & Apply</h1>
-            <p className="font-sans text-left">
+          <div className="flex flex-col items-start justify-center w-full h-full px-6 pt-32 ">
+            <h1 className="text-6xl font-bold pb-14 font-momo ">Search, Find, & Apply</h1>
+            <p className="font-sans text-left text-2xl">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus non orci
               euismod vestibulum vitae ut ex. Quisque ut arcu at lectus tristique auctor sit amet
               at turpis.
             </p>
           </div>
-          <div className="flex w-full pb-32 pt-11">
+          <div className="flex w-full pb-40 pt-20 pl-6">
             <div className="flex flex-row justify-between w-full h-auto gap-3 pl-0 bg-white rounded-2xl">
               <div className="flex items-center flex-1 px-3 py-2 mt-2 mb-2 ml-2 mr-2 bg-gray-100 rounded-lg">
                 <img src="/searchblack.svg" alt="search" className="w-5 h-5 mr-2" />
@@ -68,8 +68,8 @@ export default function Header() {
         </div>
 
 
-        <div className="flex flex-col items-center justify-start w-1/2 px-4 h-80">
-          <div className="flex flex-row items-center justify-center w-full mt-5 space-x-2">
+        <div className="flex flex-col items-end justify-start w-1/2 px-4 h-80  ">
+          <div className="flex flex-row items-center justify-end w-full mt-5 space-x-2 ">
             <button className="px-3 py-2 text-sm font-medium text-white transition-all duration-300 rounded-md hover:text-black">
               Sign In
             </button>
@@ -87,18 +87,18 @@ export default function Header() {
 
           <div className="flex flex-col justify-between w-full h-full pl-4 pr-4">
       
-            <div className="flex items-center justify-center w-full pt-20 pr-15">
-              <div className="p-3 bg-white rounded-lg shadow-md w-14 h-14 -rotate-6">
+            <div className="flex items-center justify-center w-full pt-20 pr-15 pb-11">
+              <div className="p-3 bg-white rounded-lg shadow-md w-20 h-20 -rotate-6">
                 <img src="/SearchFolder.svg" alt="searchfolder" className="inline" />
               </div>
             </div>
-            <div className="z-20 flex items-end justify-end w-full pt-20 pr-10">
-              <div className="p-4 mx-0 bg-white rounded-lg shadow-md w-14 h-14 rotate-6">
-                <FaChartBar className="text-3xl text-oceanColor" />
+            <div className=" flex items-end justify-end w-full pt-20 pl-10 ml-12 pb-11">
+              <div className="p-3 mx-0 bg-white rounded-lg shadow-md w-20 h-20 rotate-12">
+               <img src="/chatbar.svg" alt="" />
               </div>
             </div>
-            <div className="flex items-end justify-center w-full pt-16 pr-15">
-              <div className="p-3 mx-0 bg-white rounded-lg shadow-md w-14 h-14 -rotate-6">
+            <div className="flex items-end justify-center w-full pt-16 pr-16">
+              <div className="p-3 mx-0 bg-white rounded-lg shadow-md w-20 h-20 -rotate-6">
                 <img src="/protection.svg" alt="" />
               </div>
             </div>
