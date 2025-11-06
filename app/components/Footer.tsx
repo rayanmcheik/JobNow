e9f49ebaa748db018a2aa499f0556dddb1419851
rayanmcheik <rayan.m.mcheik@gmail.com>
2025-11-06 15:42:16 +0200

    done

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,16 +3,42 @@ import { FaGoogle, FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="w-full px-6 py-10 text-white bg-oceanColor mx-auto">
+    <footer className="w-full px-6 py-10 text-white bg-oceanColor mx-auto pt-56">
       <div className="max-w-[1200px] mx-auto grid grid-cols-1 md:grid-cols-4 md:gap-10 justify-between">
         <div className="flex flex-col justify-center items-start">
-          <img src='logo2.png' className='w-[85%] h-20 mb-4' />
-          <p className="mb-4 space-y-2 text-gray-100">
+          <img src='logo2.png' className='md:w-[85%] h-20 mb-4 w-[45%]' />
+          <p className="mb-4 space-y-2 text-gray-100 pb-10 md:pb-0">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus non orci
             euismod vestibulum vitae ut ex.
           </p>
         </div>
-        <div>
+       
+        <div className='hidden md:block'>
+
+          <h3 className="mb-4 text-3xl font-bold font-poppins">Company</h3>
+          <ul className="space-y-2 text-gray-100">
+            <li><a href="#" className="hover:text-white">Address: 123 Fifth Avenue, New York -1060, USA</a></li>
+            <li><a href="#" className="hover:text-white">Call Us: ‪+(1600) 456 7890‬</a></li>
+            <li><a href="#" className="hover:text-white">Email: yourid @example.com</a></li>
+            <li><a href="#" className="hover:text-white">Mon Sot: 9:00 AM - 19:00 PM</a></li>
+          </ul>
+        </div>
+        <div className='hidden md:block md:w-auto md:ml-20'>
+          <h3 className="mb-4 text-3xl font-bold font-poppins text-left">Pages</h3>
+          <ul className="space-y-2 text-gray-100 text-left ">
+            <li><a href="#" className="hover:text-white"> &gt; Home</a></li>
+            <li><a href="#" className="hover:text-white"> &gt; Booking</a></li>
+            <li><a href="#" className="hover:text-white"> &gt; Facilities</a></li>
+            <li><a href="#" className="hover:text-white"> &gt; About Us</a></li>
+            <li><a href="#" className="hover:text-white"> &gt; Location</a></li>
+            <li><a href="#" className="hover:text-white"> &gt; Contact</a></li>
+          </ul>
+        </div>
+
+
+
+ <div className='flex flex-row justify-between w-full md:hidden gap-10'>
+   <div>
 
           <h3 className="mb-4 text-3xl font-bold font-poppins">Company</h3>
           <ul className="space-y-2 text-gray-100">
@@ -25,16 +51,17 @@ const Footer = () => {
         <div className=''>
           <h3 className="mb-4 text-3xl font-bold font-poppins text-center">Pages</h3>
           <ul className="space-y-2 text-gray-100 text-center">
-            <li><a href="#" className="hover:text-white"> Home</a></li>
-            <li><a href="#" className="hover:text-white">Booking</a></li>
-            <li><a href="#" className="hover:text-white">Facilities</a></li>
-            <li><a href="#" className="hover:text-white">About Us</a></li>
-            <li><a href="#" className="hover:text-white">Location</a></li>
-            <li><a href="#" className="hover:text-white">Contact</a></li>
+            <li><a href="#" className="hover:text-white"> &gt; Home</a></li>
+            <li><a href="#" className="hover:text-white"> &gt; Booking</a></li>
+            <li><a href="#" className="hover:text-white"> &gt; Facilities</a></li>
+            <li><a href="#" className="hover:text-white"> &gt; About Us</a></li>
+            <li><a href="#" className="hover:text-white"> &gt; Location</a></li>
+            <li><a href="#" className="hover:text-white"> &gt; Contact</a></li>
           </ul>
         </div>
+ </div>
         <div>
-          <h3 className="mb-4 text-3xl font-bold font-poppins">Contact Us</h3>
+          <h3 className="mb-4 text-3xl font-bold font-poppins pt-10 md:pt-0">Contact Us</h3>
           <div className="flex space-x-4 text-gray-100">
             <a
               href="https://www.google.com"
